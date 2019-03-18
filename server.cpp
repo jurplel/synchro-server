b461e72b7551067855e0a6eb62527463ecb56f84
Benjamin O <jeep70cp@gmail.com>
2019-03-18 13:25:38 -0400

    fixed readonly error

--- server.cpp
+++ server.cpp
@@ -18,7 +18,7 @@ void Server::clientConnected()
 {
     auto socket = server->nextPendingConnection();
     QByteArray dataBlock;
-    QDataStream dataBlockStream(dataBlock);
+    QDataStream dataBlockStream(&dataBlock, QIODevice::WriteOnly);
     dataBlockStream << quint8(command::pause);
     dataBlockStream.device()->seek(0);
     dataBlockStream << quint16(sizeof(dataBlock) - sizeof(quint16));
